2fd6b531b5068e5a22d809ca577e31e0579fd072
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-02-10 18:30:45 +0530

    .

--- ETL PIPELINE/dags/ETLWeatherApi.py
+++ ETL PIPELINE/dags/ETLWeatherApi.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.providers.https.hooks.http import HttpHook
+from airflow.providers.http.hooks.http import HttpHook 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.decorators import task
 from airflow.utils.dates import days_ago 
